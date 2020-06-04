01ac9a7bfa7ac2c81029cb2ddaa533db35a5e589
Jonathan Bravo <jonathan.bravo@rappi.com>
2020-06-04 12:49:52 -0500

    Initial commit

---
+++ app/src/main/java/com/jonhdev/dogs/di/AppComponent.kt
@@ -0,0 +1,27 @@
+package com.jonhdev.dogs.di
+
+import android.app.Application
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(appController: AppController)
+
+}
